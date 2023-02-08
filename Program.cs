882b4cc3f03b0bbb4c1b383dcee7ec529a3c0755
Irina <iirna@yahoo.com>
2023-02-08 23:41:48 +0300

    Решение 23

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
-﻿Console.WriteLine("Задача 19");
-Console.Write("Введите число: ");
-string? number = Console.ReadLine();
+﻿Console.WriteLine("\n Задача 21");
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
 
-void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
-    Console.WriteLine($"Ваше число: {number} - палиндром.");
-  }
-  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
 }
 
-if (number!.Length == 5){
-  CheckingNumber(number);
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
 }
-else Console.WriteLine($"Введи правильное число");
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
