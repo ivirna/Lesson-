9a0f03255c93a9f8337e66714bf9782434e1180e
Irina <iirna@yahoo.com>
2023-02-12 18:02:49 +0300

    Решение 36

--- Program.cs
+++ Program.cs
@@ -1,32 +1,31 @@
-﻿Console.WriteLine("Задача 34");
+﻿Console.WriteLine($"\nЗадача 36");
 Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
 Console.WriteLine("Вот наш массив: ");
 PrintArray(numbers);
-int count = 0;
+int sum = 0;
 
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
 
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
 
 void FillArrayRandomNumbers(int[] numbers)
 {
     for(int i = 0; i < numbers.Length; i++)
-    {
-        numbers[i] = new Random().Next(100,1000);
-    }
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
 }
 void PrintArray(int[] numbers)
 {
     Console.Write("[ ");
     for(int i = 0; i < numbers.Length; i++)
-    {
-        Console.Write(numbers[i] + " ");
-    }
+        {
+            Console.Write(numbers[i] + " ");
+        }
     Console.Write("]");
     Console.WriteLine();
 }
