b90c61c2c47411720a1a8dad63656feed1b9b2b7
Irina <iirna@yahoo.com>
2023-02-19 15:58:09 +0300

    Решение 50

--- Program.cs
+++ Program.cs
@@ -1,39 +1,43 @@
-﻿Console.WriteLine($"\nЗадача 47");
-Console.Write("Введите m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите n: ");
+﻿Console.WriteLine($"\nЗадача 50");
+Console.WriteLine("введите номер строки");
 int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
 
-Console.Clear();
-Console.WriteLine($"m = {m}, n = {n}.");
-
-double[,] array = new double[m, n];
-
-CreateArrayDouble(array);
-
-WriteArray(array);
-
-Console.WriteLine();
-
-void CreateArrayDouble(double[,] array)
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
 {
-  for (int i = 0; i < m; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      array[i, j] = new Random().NextDouble() * 20 - 10;
-    }
-  }
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
 }
 
-void WriteArray (double[,] array){
-for (int i = 0; i < m; i++)
-  {
-      for (int j = 0; j < n; j++)
-      {
-        double alignNumber = Math.Round(array[i, j], 1);
-        Console.Write(alignNumber + " ");
-      }
-      Console.WriteLine();
-  }
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
 }
