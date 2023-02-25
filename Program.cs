cfff3f385d124180ab59c78ed82408edc6224846
Irina <iirna@yahoo.com>
2023-02-25 12:05:25 +0300

    решение 64

--- Program.cs
+++ Program.cs
@@ -1,50 +1,19 @@
-﻿Console.WriteLine($"\nЗадача 52");
-Console.WriteLine("введите количество строк");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите количество столбцов");
-int m = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine($"\nЗадача 64");
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
 
-int[,] numbers = new int[n, m];
-FillArrayRandomNumbers(numbers);
-
-
-for (int j = 0; j < numbers.GetLength(1); j++)
+void PrintNumber(int n, int count)
 {
-    double avarage = 0;
-    for (int i = 0; i < numbers.GetLength(0); i++)
-    {
-        avarage = (avarage + numbers[i, j]);
-    }
-    avarage = avarage / n;
-    Console.Write(avarage + "; ");
-}
-Console.WriteLine();
-PrintArray(numbers);
-
-
-
-void FillArrayRandomNumbers(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(0, 10);
-        }
-    }
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write(count + ", ");
 }
 
-void PrintArray(int[,] array)
+int InputNumbers(string input) 
 {
-
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        Console.Write("[ ");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.Write("]");
-        Console.WriteLine("");
-    }
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
 }
